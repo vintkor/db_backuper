60e7d83c9d814ac8c1df3dbc8ff19db9be172538
alexandr_k <Alexandr.K@enkonix.com>
2021-12-24 18:55:41 +0200

    Creon work

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ WORKDIR ${APP_DIR}
 
 RUN apk add --update python3 py-pip apk-cron && rm -rf /var/cache/apk/*
 
-ADD ./requirements.txt .
+ADD ./app/requirements.txt .
 RUN pip3 install --ignore-installed six && pip3 install -r requirements.txt
 COPY ./app .
 
--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ Backup PostgreSQL database to AWS S3.
 
 import datetime
 import os
+import boto3
 
-# import boto3
 
-
-# S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-# BACKUP_KEY_PUB_FILE = os.getenv("BACKUP_KEY_PUB_FILE")
-# TIME_ZONE = os.getenv("TIME_ZONE", "Europe/Moscow")
+AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
 
 
 class Dumper:
@@ -18,6 +18,8 @@ class Dumper:
     def __init__(self):
         self.DB_CONTAINER_NAME = os.getenv('DB_CONTAINER_NAME')
         self.DB_USER_NAME = os.getenv('DB_USER_NAME')
+        self.POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+        self.POSTGRES_DB = os.getenv('POSTGRES_DB')
         self.filename = self._get_now_datetime_str()
 
     @staticmethod
@@ -33,39 +35,36 @@ class Dumper:
 
     def dump_database(self):
         print("📦 Preparing database backup started")
-        command = f'docker exec -t {self.DB_CONTAINER_NAME} pg_dumpall -c -U {self.DB_USER_NAME} > {self.filename}'
+        command = f'docker exec -i {self.DB_CONTAINER_NAME} /bin/bash -c ' \
+                  f'"PGPASSWORD={self.POSTGRES_PASSWORD} ' \
+                  f'pg_dump --username {self.DB_USER_NAME} {self.POSTGRES_DB}" > {self.filename}'
+
         dump_db_operation_status = os.WEXITSTATUS(os.system(command))
 
         if dump_db_operation_status != 0:
             exit(f"\U00002757 Dump database command exits with status {dump_db_operation_status}.")
 
-        print("\U0001F510 DB dumped, archieved and encoded")
+        print("\U0001F510 DB dumped")
+
+    def load_to_s3(self):
+        client = boto3.client(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        with open(self.filename, 'rb') as data:
+            client.upload_fileobj(data, AWS_S3_BUCKET_NAME, self.filename)
+
+        print("\U0001f680 Uploaded")
 
     def remove_temp_files(self):
         os.remove(self.filename)
         print("\U0001F44D That's all!")
-
-# def get_s3_instance():
-#     session = boto3.session.Session()
-#     return session.client(
-#         service_name='s3',
-#         endpoint_url='https://storage.yandexcloud.net'
-#     )
-#
-#
-# def upload_dump_to_s3():
-#     print("\U0001F4C2 Starting upload to Object Storage")
-#     get_s3_instance().upload_file(
-#         Filename=DB_FILENAME,
-#         Bucket=S3_BUCKET_NAME,
-#         Key=f'db-{get_now_datetime_str()}.sql.gz.enc'
-#     )
-#     print("\U0001f680 Uploaded")
-#
 
 
 if __name__ == "__main__":
     dumper = Dumper()
     dumper.say_hello()
     dumper.dump_database()
-    # dumper.remove_temp_files()
+    dumper.load_to_s3()
+    dumper.remove_temp_files()
